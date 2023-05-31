3768189fcf9128a9cb7bf685fa063965ef083f1e
Cisco <ciscophrenzy@gmail.com>
2023-05-31 09:56:54 -0600

    Update components and add better styling

--- src/Redux/coin/coinSlice.js
+++ src/Redux/coin/coinSlice.js
@@ -1,27 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
-// Define the async thunk to fetch coin data
 export const fetchCoins = createAsyncThunk('coins/fetchCoins', async () => {
-  const response = await axios.get('https://api.coincap.io/v2/assets?limit=100');
-  return response.data.data;
+  const response = await fetch('https://api.coincap.io/v2/assets?limit=100');
+  const res = response.json();
+  return res;
 });
 
 export const searchCoins = createAsyncThunk(
   'coins/searchCoins',
   async (searchQuery, { rejectWithValue }) => {
     try {
-      const response = await axios.get(
+      const response = await fetch(
         `https://api.coincap.io/v2/assets?search=${searchQuery}`,
       );
-      return response.data.data;
+      const res = response.json();
+      return res;
     } catch (error) {
       return rejectWithValue(error.response.data);
     }
   },
 );
 
-// Create the coin slice
 const coinSlice = createSlice({
   name: 'coins',
   initialState: {
@@ -31,19 +30,16 @@ const coinSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    // Handle the fetchCoins.pending action
     builder.addCase(fetchCoins.pending, (state) => {
       state.loading = true;
       state.error = null;
     });
 
-    // Handle the fetchCoins.fulfilled action
     builder.addCase(fetchCoins.fulfilled, (state, action) => {
       state.loading = false;
       state.coins = action.payload;
     });
 
-    // Handle the fetchCoins.rejected action
     builder.addCase(fetchCoins.rejected, (state, action) => {
       state.loading = false;
       state.error = action.error.message;
@@ -67,5 +63,4 @@ const coinSlice = createSlice({
   },
 });
 
-// Export the async thunk and the coin reducer
 export const { actions: coinActions, reducer: coinReducer } = coinSlice;
